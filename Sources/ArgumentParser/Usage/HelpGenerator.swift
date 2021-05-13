618848b84a484c59db66692bb878c3a9982ecbdc
KOTARO SUTO <ng1a5ss43@gmail.com>
2021-05-13 19:39:37 +0900

    fix

--- Sources/ArgumentParser/Usage/HelpGenerator.swift
+++ Sources/ArgumentParser/Usage/HelpGenerator.swift
@@ -122,8 +122,8 @@ internal struct HelpGenerator {
       let string = UsageGenerator(toolName: toolName, definition: [ArgumentSet(defaultSubcommand)]).synopsisWithoutToolName
       if string.count > 0 {
         usageString = "\n" +  String(repeating: " ", count: HelpGenerator.helpIndent)  + toolName + " " + string
+        usageString += "\n" + String(repeating: " ", count: HelpGenerator.helpIndent)
       }
-      usageString += "\n" + String(repeating: " ", count: HelpGenerator.helpIndent)
       self.commandStack.append(defaultSubcommand)
     }
     // Search for the default command
