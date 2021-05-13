244cfc231df17f4a1660e4355fa05e47fa3cc8a9
KOTARO SUTO <ng1a5ss43@gmail.com>
2021-05-13 23:18:09 +0900

    fix

--- Tests/ArgumentParserExampleTests/MathExampleTests.swift
+++ Tests/ArgumentParserExampleTests/MathExampleTests.swift
@@ -34,7 +34,7 @@ final class MathExampleTests: XCTestCase {
           -x, --hex-output        Use hexadecimal notation for the result.
           --version               Show the version.
           -h, --help              Show help information.
-        
+
         SUBCOMMANDS:
           add (default)           Print the sum of the values.
           multiply                Print the product of the values.
